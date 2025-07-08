1fd946f25400223f0ff118399885aa30dec1f559
Atri Chandra <121769804+AtriChandra@users.noreply.github.com>
2025-07-08 14:01:19 +0530

    Change in update.js

--- update.js
+++ update.js
@@ -1,1 +1,14 @@
-console.log("update");
+const dbConnect=require('./mongodb');
+
+const updateData=async()=>{
+    let data=await dbConnect();
+    let result= await data.updateMany(
+        {brand: 'Iphone 16'},
+        {
+            $set:{name: 'Iphone 16 Pro', price: 1500}
+        }
+    )
+    console.log(result);
+}
+
+updateData();
